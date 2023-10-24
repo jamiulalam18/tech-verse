8b06a526506f943eae24df34ccb81e42990f0ea4
jamiulalam18 <jamiulalam18@gmail.com>
2023-10-24 04:46:56 -0400

    Product add/modify function

--- src/Components/AddProductForm/AddProductForm.jsx
+++ src/Components/AddProductForm/AddProductForm.jsx
@@ -5,16 +5,54 @@ import {
   Textarea,
   TextInput,
 } from "flowbite-react";
+import { successToast } from "../Toasts/SuccessToast";
 const AddProductForm = () => {
+
+  const handleAddProduct =(e) =>{
+    e.preventDefault();
+    const form=new FormData(e.currentTarget);
+    const brand_name=form.get('brand_name');
+    const product_name=form.get('product_name');
+    const product_category=form.get('product_category');
+    const product_image_url1=form.get('product_image_url1');
+    const product_image_url2=form.get('product_image_url2');
+    const product_price=form.get('product_price');
+    const product_rating=form.get('product_rating');
+    const product_short_description=form.get('short_description');
+    const product_long_description=form.get('long_description');
+    const product_highlights_txt=form.get('product_highlights');
+    const product_image=[product_image_url1,product_image_url2]
+    const product_highlights=product_highlights_txt.split('\n').filter(line => line.trim() !== '');
+
+    const new_product={brand_name,product_name,product_category,product_image,product_highlights,product_price,product_rating,product_short_description,product_long_description}
+    console.log(new_product);
+
+    // send data to the server
+    fetch('http://localhost:5000/products', {
+      method: 'POST',
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify(new_product)
+  })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data);
+          if(data.insertedId){
+              successToast("Product inserted successfully!!")
+          }
+      })
+    
+  }
   return (
     <div className="bg-white flex justify-center w-full rounded-lg">
-      <form className="flex max-w-md flex-col gap-2 w-full py-4 rounded-lg">
+      <form onSubmit={handleAddProduct} className="flex max-w-md flex-col gap-2 w-full py-4 rounded-lg">
         <div>
           <div className="max-w-md" id="select">
             <div className=" block">
               <Label htmlFor="brand_name" value="Select Brand" />
             </div>
-            <Select id="brand_name" required>
+            <Select id="brand_name" name="brand_name" required>
               <option>Apple Inc.</option>
               <option>Samsung Electronics Co., Ltd.</option>
               <option>Huawei Technologies Co., Ltd.</option>
@@ -31,6 +69,7 @@ const AddProductForm = () => {
           </div>
           <TextInput
             id="product_name"
+            name="product_name"
             placeholder="Product Name"
             required
             type="text"
@@ -40,9 +79,9 @@ const AddProductForm = () => {
         <div>
           <div className="max-w-md" id="select">
             <div className=" block">
-              <Label htmlFor="brand_name" value="Select Product Category" />
+              <Label htmlFor="product_category" value="Select Product Category" />
             </div>
-            <Select id="brand_name" required>
+            <Select id="product_category" name="product_category" required>
               <option>2-in-1 Tablet</option>
               <option>3D Printer</option>
               <option>All-in-One PC</option>
@@ -89,6 +128,7 @@ const AddProductForm = () => {
           </div>
           <TextInput
             id="product_image_url1"
+            name="product_image_url1"
             placeholder="Product Image URL 1"
             required
             type="text"
@@ -101,6 +141,7 @@ const AddProductForm = () => {
           </div>
           <TextInput
             id="product_image_url2"
+            name="product_image_url2"
             placeholder="Product Image URL 2"
             required
             type="text"
@@ -113,8 +154,12 @@ const AddProductForm = () => {
           </div>
           <TextInput
             id="product_price"
+            name="product_price"
             placeholder="0.00"
             required
+            min={0.0}
+            max={100000.00}
+            step="0.01"
             type="number"
           />
         </div>
@@ -125,16 +170,18 @@ const AddProductForm = () => {
           </div>
           <TextInput
             id="product_rating"
+            name="product_rating"
             placeholder="0.0"
             max={5.0}
             min={0.0}
+            step="0.01"
             required
             type="number"
           />
         </div>
 
         <div>
-          <div className="max-w-md" id="textarea">
+          <div className="max-w-md" id="short_description">
             <div className="block">
               <Label
                 htmlFor="short_description"
@@ -143,6 +190,7 @@ const AddProductForm = () => {
             </div>
             <Textarea
               id="short_description"
+              name="short_description"
               placeholder="Write a short description..."
               required
               rows={2}
@@ -151,7 +199,7 @@ const AddProductForm = () => {
         </div>
 
         <div>
-          <div className="max-w-md" id="textarea">
+          <div className="max-w-md" id="long_description">
             <div className="block">
               <Label
                 htmlFor="long_description"
@@ -160,6 +208,7 @@ const AddProductForm = () => {
             </div>
             <Textarea
               id="long_description"
+              name="long_description"
               placeholder="Write a detail description of the product..."
               required
               rows={4}
@@ -168,7 +217,7 @@ const AddProductForm = () => {
         </div>
 
         <div>
-          <div className="max-w-md" id="textarea">
+          <div className="max-w-md" id="product_highlights">
             <div className="block">
               <Label
                 htmlFor="product_highlights"
@@ -177,6 +226,7 @@ const AddProductForm = () => {
             </div>
             <Textarea
               id="product_highlights"
+              name="product_highlights"
               placeholder="Write product highlights separated by new lines..."
               required
               rows={4}
--- src/Components/UpdateProductForm/UpdateProductForm.jsx
+++ src/Components/UpdateProductForm/UpdateProductForm.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Button, Label, Select, Textarea, TextInput } from "flowbite-react";
+import { successToast } from './../Toasts/SuccessToast';
 
 const UpdateProductForm = ({ product }) => {
   const {
@@ -14,22 +15,57 @@ const UpdateProductForm = ({ product }) => {
     product_price,
     product_rating,
   } = product;
+
+  const handleUpdateProduct=(e)=>{
+    e.preventDefault();
+    const form=new FormData(e.currentTarget);
+    const brand_name=form.get('brand_name');
+    const product_name=form.get('product_name');
+    const product_category=form.get('product_category');
+    const product_image_url1=form.get('product_image_url1');
+    const product_image_url2=form.get('product_image_url2');
+    const product_price=form.get('product_price');
+    const product_rating=form.get('product_rating');
+    const product_short_description=form.get('short_description');
+    const product_long_description=form.get('long_description');
+    const product_highlights_txt=form.get('product_highlights');
+    const product_image=[product_image_url1,product_image_url2]
+    const product_highlights=product_highlights_txt.split('\n').filter(line => line.trim() !== '');
+
+    const updated_product={brand_name,product_name,product_category,product_image,product_highlights,product_price,product_rating,product_short_description,product_long_description}
+    console.log(updated_product);
+
+    // send data to the server
+    fetch(`http://localhost:5000/products/${_id}`, {
+      method: 'PUT',
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify(updated_product)
+  })
+      .then(res => res.json())
+      .then(data => {
+          console.log(data);
+          if (data.modifiedCount > 0) {
+              successToast("Product updated successfully!!!");
+          }
+      })
+  };
   return (
     <div className="bg-white flex justify-center w-full rounded-lg">
-      <form className="flex max-w-md flex-col gap-2 w-full py-4 rounded-lg">
+      <form onSubmit={handleUpdateProduct} className="flex max-w-md flex-col gap-2 w-full py-4 rounded-lg">
         <div>
           <div className="max-w-md" id="select">
             <div className=" block">
               <Label htmlFor="brand_name" value="Select Brand" />
             </div>
-            <Select id="brand_name" required>
+            <Select id="brand_name" name="brand_name" defaultValue={brand_name} required>
               <option>Apple Inc.</option>
               <option>Samsung Electronics Co., Ltd.</option>
               <option>Huawei Technologies Co., Ltd.</option>
               <option>Microsoft Corporation</option>
               <option>Google LLC</option>
               <option>ASUS Corporation</option>
-              <option selected>{brand_name}</option>
             </Select>
           </div>
         </div>
@@ -40,18 +76,20 @@ const UpdateProductForm = ({ product }) => {
           </div>
           <TextInput
             id="product_name"
-            placeholder={product_name}
+            name="product_name"
+            placeholder="Product Name"
             required
             type="text"
+            defaultValue={product_name}
           />
         </div>
 
         <div>
           <div className="max-w-md" id="select">
             <div className=" block">
-              <Label htmlFor="brand_name" value="Select Product Category" />
+              <Label htmlFor="product_category" value="Select Product Category" />
             </div>
-            <Select id="brand_name" required>
+            <Select id="product_category" name="product_category" required defaultValue={product_category}>
               <option>2-in-1 Tablet</option>
               <option>3D Printer</option>
               <option>All-in-One PC</option>
@@ -88,7 +126,6 @@ const UpdateProductForm = ({ product }) => {
               <option>Wireless Charger</option>
               <option>Wireless Earbuds</option>
               <option>Wireless Headphones</option>
-              <option selected>{product_category}</option>
             </Select>
           </div>
         </div>
@@ -99,9 +136,11 @@ const UpdateProductForm = ({ product }) => {
           </div>
           <TextInput
             id="product_image_url1"
-            placeholder={product_image[0]}
+            name="product_image_url1"
+            placeholder="Product Image URL 1"
             required
             type="text"
+            defaultValue={product_image[0]}
           />
         </div>
 
@@ -111,9 +150,11 @@ const UpdateProductForm = ({ product }) => {
           </div>
           <TextInput
             id="product_image_url2"
-            placeholder={product_image[1]}
+            name="product_image_url2"
+            placeholder="Product Image URL 2"
             required
             type="text"
+            defaultValue={product_image[1]}
           />
         </div>
 
@@ -123,9 +164,14 @@ const UpdateProductForm = ({ product }) => {
           </div>
           <TextInput
             id="product_price"
-            placeholder={product_price}
+            name="product_price"
+            placeholder="0.00"
             required
+            min={0.0}
+            max={100000.00}
+            step="0.01"
             type="number"
+            defaultValue={product_price}
           />
         </div>
 
@@ -135,16 +181,19 @@ const UpdateProductForm = ({ product }) => {
           </div>
           <TextInput
             id="product_rating"
-            placeholder={product_rating}
+            name="product_rating"
+            placeholder="0.0"
             max={5.0}
             min={0.0}
+            step="0.01"
             required
             type="number"
+            defaultValue={product_rating}
           />
         </div>
 
         <div>
-          <div className="max-w-md" id="textarea">
+          <div className="max-w-md" id="short_description">
             <div className="block">
               <Label
                 htmlFor="short_description"
@@ -153,15 +202,17 @@ const UpdateProductForm = ({ product }) => {
             </div>
             <Textarea
               id="short_description"
-              placeholder={product_short_description}
+              name="short_description"
+              placeholder="Write a short description..."
               required
               rows={2}
+              defaultValue={product_short_description}
             />
           </div>
         </div>
 
         <div>
-          <div className="max-w-md" id="textarea">
+          <div className="max-w-md" id="long_description">
             <div className="block">
               <Label
                 htmlFor="long_description"
@@ -170,28 +221,35 @@ const UpdateProductForm = ({ product }) => {
             </div>
             <Textarea
               id="long_description"
-              placeholder={product_long_description}
+              name="long_description"
+              placeholder="Write a detail description of the product..."
               required
               rows={4}
+              defaultValue={product_long_description}
             />
           </div>
         </div>
 
         <div>
-          <div className="max-w-md" id="textarea">
+          <div className="max-w-md" id="product_highlights">
             <div className="block">
-              <Label htmlFor="product_highlights" value="Product Highlights" />
+              <Label
+                htmlFor="product_highlights"
+                value="Product Highlights"
+              />
             </div>
             <Textarea
               id="product_highlights"
-              placeholder={product_highlights}
+              name="product_highlights"
+              placeholder="Write product highlights separated by new lines..."
               required
               rows={4}
+              defaultValue={product_highlights}
             />
           </div>
         </div>
 
-        <Button type="submit">Update</Button>
+        <Button type="submit">Add Product</Button>
       </form>
     </div>
   );
